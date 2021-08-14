60be44cdac030d6a2bef323a7138bdfd3ed76bf5
Advik-B <advik.b@gmail.com>
2021-08-14 10:44:06 +0530

    Update versions_link.py

--- app/versions_link.py
+++ app/versions_link.py
@@ -1,6 +1,3 @@
 class bukkit():
     def get_link(self , version_number:str) -> str:
-        versions = {
-            
-            "1.8.9":"https://dl.google.com"
-        }
+        versions = {}
