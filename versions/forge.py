574e256cf25f9381e0d8d5f1f18a7335e0549683
Advik-B <advik.b@gmail.com>
2021-08-15 17:52:12 +0530

    Changed the path of everything (just a small update) (stable)

---
+++ versions/forge.py
@@ -0,0 +1,20 @@
+from default import *
+
+class Forge(Server):
+    def getlink(version_number:str) -> str:
+        global versions
+
+        versions = {}
+
+        versions['stable'] = versions['1.12']
+        versions['dev'] = versions['1.12.2']
+
+        version = versions.keys()
+        if version_number in version:
+            download_url = versions[version_number]
+            return download_url
+        else:
+            print()
+            print(f'The version "{version_number}" is not found!')
+            print()
+            return "about:blank"
