27e6f768eda40d8206b05e36159fa9e9806386d9
Advik-B <advik.b@gmail.com>
2021-08-15 17:37:07 +0530

    Updated the .gitignore and also made the versions  object oriented

--- app/versions/paper.py
+++ app/versions/paper.py
@@ -1,10 +1,6 @@
-import subprocess
-import os
-import requests
-from bs4 import BeautifulSoup
+from default import *
 
-
-class Paper():
+class Paper(Server):
     def getlink(version_number:str) -> str:
         global versions
 
@@ -39,61 +35,3 @@ class Paper():
             print(f'The version "{version_number}" is not found!')
             print()
             return "about:blank"
-
-    def download(link:str , folder_path=None)  -> None:
-
-
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600',
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-            }
-        MediaUrl = link
-
-        url = MediaUrl
-        req = requests.get(url, headers)
-        soup = BeautifulSoup(req.content, 'html.parser')
-
-        url = soup.find("a", class_="popsok").get('href')
-        r = requests.get(url)
-        if folder_path != None:
-            file_path = folder_path.__add__('/server.jar').replace('\\' , '/')
-        else:
-            file_path = ('./server.jar')
-
-        print ("File Name : " + soup.find("div", class_="filename").get_text())
-        print (soup.find("ul", class_="details").get_text())
-
-        with open(file_path, 'wb') as f:
-            f.write(r.content)
-            print('Sucessfully downloaded the server file!')
-
-    def runserver(server_folder=None , run_command=None , server_file_name=None) -> None:
-        cwd = os.getcwd()
-        path = server_folder
-        eula = str(os.path.join(path , 'eula.txt')).replace('\\' , '/')
-        global eula_content
-
-        if server_folder == None:
-            if server_file_name == None:
-                server_file_name = 'server.jar'
-            if run_command == None:
-                run_command = 'java -Xmx1024M -Xms1024M -jar'  
-                  
-            eula_content = "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).\n#Sun Aug 15 09:55:51 IST 2021\neula=true"
-            with open(eula, mode='w+') as f:
-                f.write(eula_content)
-
-            subprocess.Popen(f'{run_command} {server_file_name} nogui' , cwd=(os.getcwd()))
-        elif server_folder != None:
-            if server_file_name == None:
-                server_file_name = 'server.jar'
-
-            if run_command == None:
-                run_command = 'java -Xmx1024M -Xms1024M -jar'
-            eula_content = "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).\n#Sun Aug 15 09:55:51 IST 2021\neula=true"
-            with open(eula, mode='w+') as f:
-                f.write(eula_content)
-            subprocess.Popen(f'{run_command} {server_file_name} nogui' , cwd=path)
