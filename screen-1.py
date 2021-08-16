73f99aed64c42da2d929675945de2abb5c283307
Advik-B <advik.b@gmail.com>
2021-08-16 15:48:20 +0530

    Working on the main file

---
+++ screen-1.py
@@ -0,0 +1,12 @@
+from versions.default import *
+from tkinter import *
+from tkinter import ttk
+import pygame.mixer
+
+pygame.mixer.init()
+root = Tk()
+root.title('Server installer')
+root.geometry('500x650')
+root.iconbitmap('./assets/pictures/icon.ico')
+
+root.mainloop()
