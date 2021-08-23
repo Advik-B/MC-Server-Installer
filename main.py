2394a04fb9623e950670efed931d1f8c00645eb6
Advik-B <advik.b@gmail.com>
2021-08-23 15:24:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
 from tkinter import *
 from tkinter import ttk , messagebox , filedialog
 from tkinter.font import Font
-import sounds
 from font import *
+import sounds
+import os
 root = Tk()
 class Fonts():
-        Ubuntu_mono = Font(
-            family='Ubuntu Mono',
+    Ubuntu_mono = Font(
+        family='Ubuntu Mono',
+        weight='normal',
+        slant='roman',
+        underline=0,
+        overstrike=0,
+        size=24
+        )
+
+    Bahnschrift = Font(
+        family='Bahnschrift',
+        weight='normal',
+        slant='roman',
+        underline=0,
+        overstrike=0,
+        size=24
+        )
+    
+    Vendara = Font(
+            family='Vendara',
             weight='normal',
             slant='roman',
             underline=0,
-            overstrike=0,
             size=24
             )
 
-        Bahnschrift = Font(
-            family='Bahnschrift',
+    Vendara_mini = Font(
+            family='Vendara',
             weight='normal',
             slant='roman',
             underline=0,
-            overstrike=0,
-            size=24
+            size=15
             )
+
+    Ubuntu_mono_mini = Font(
+        family='Ubuntu Mono',
+        weight='normal',
+        slant='roman',
+        underline=0,
+        overstrike=0,
+        size=12
+        )
 
 root.geometry('750x500')
-my_label = ttk.Label(root,  text='Welcome', font=Fonts.Bahnschrift)
-my_label.pack()
-root.mainloop()
+Agreement_Heading = ttk.Label(root,  text='Please read and accept the licence agreement below to continue.', font=Fonts.Vendara_mini)
+Agreement = ttk.Entry(root)
+
+
+if __name__ == '__main__':
+    Agreement_Heading.pack()
+    Agreement.pack(pady=20)
+
+    root.mainloop()
