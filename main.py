e733f1e2921dd0da2c62a033b38d9d32b2806fb0
Advik-B <advik.b@gmail.com>
2021-08-23 14:48:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,28 @@ from tkinter import *
 from tkinter import ttk , messagebox , filedialog
 from tkinter.font import Font
 import sounds
+from font import *
 root = Tk()
-root.geometry('750x500')
-Ubuntu_mono = Font(
-    family='Ubuntu Mono',
-    weight='normal',
-    slant='roman',
-    underline=0,
-    overstrike=0)
+class Fonts():
+    def __init__(self) -> None:
+        Ubuntu_mono = Font(
+            family='Ubuntu Mono',
+            weight='normal',
+            slant='roman',
+            underline=0,
+            overstrike=0,
+            size=24
+            )
 
-my_label = ttk.Button(root,  text='Hello 0' , command=sounds.button , width=20)
+        Bahnschrift = Font(
+            family='Bahnschrift',
+            weight='normal',
+            slant='roman',
+            underline=0,
+            overstrike=0,
+            size=24
+            )
+root.geometry('750x500')
+my_label = ttk.Label(root,  text='Welcome', font=Bahnschrift)
 my_label.pack()
 root.mainloop()
