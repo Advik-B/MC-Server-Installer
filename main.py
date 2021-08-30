c91884c4cc98775f918c5dc320ae9792abd43b5e
Advik-B <advik.b@gmail.com>
2021-08-30 17:23:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,35 @@ from versions.spigot import *
 from versions.default import *
 from versions.mohist import *
 from function.most_used import *
+from sounds import *
 from getpass import getpass
+
+def clear() -> None:
+    os.system("cls")
 
 def eula() -> str:
     with open('./LICENSE' , mode='r') as f:
-        content = f'{"Please read and accept the license agreement below to continue.".center(80)}\n\n{f.read()}'
+        content = f'{"Please read and accept the license agreement below to continue.".center(80)}\n\n\n{f.read()}'
         print(content.center(80))
         return content
 
-def fail():
+def failed_to_agree() -> 1:
     print()
     __str = f'-^-^-^-^-^-^-^-^-^-YOU NEED TO AGREE TO THE ABOVE AGREEMENT!-^-^-^-^-^-^-^-^-^-'
     getpass(__str.center(80))
+    return 1
+
 
 eula()
-
-if  str(prinput('Do agree to the above license/agreement [Y/N]')).casefold() == 'n':
-    exit(fail())
-else:
-    print('ahdegauydgewg')
+while True:
+    answer = str(prinput('Do agree to the above license/agreement [Y/N]')).casefold()
+    if  answer == 'n':
+        exit(failed_to_agree())
+    elif answer == 'y':
+        print()
+        break
+    else:
+        clear()
+        error()
+        eula()
+        continue
