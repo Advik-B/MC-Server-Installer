efde264c42ca245e63fa5939d9510012f2a8b210
Advik <advik.b@gmail.com>
2021-10-26 18:09:37 +0100

    Add a temp `dict` to write to json files

--- config.py
+++ config.py
@@ -1,3 +1,13 @@
 #! This is the config file. 
 #! DO NOT CHANGE IT UNLESS YOU KNOW WHAT YOU ARE DOING!
+#! NOTE: I get all the links form https://mcversions.net/
 #TODO: Add some config shit here
+
+#TEMP: BEGINING: All the code below this is temp
+vanilla = {
+    
+    "1.17.1":'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
+    '1.17':'https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar',
+    
+}
+#TEMP: END: All the code form this line starts counting
