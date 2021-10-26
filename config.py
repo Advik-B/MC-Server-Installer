00243d9b0dc0671c1586d7e373de8fa4676517bd
Advik <advik.b@gmail.com>
2021-10-26 18:29:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 #! This is the config file. 
 #! DO NOT CHANGE IT UNLESS YOU KNOW WHAT YOU ARE DOING!
-#! NOTE: I get all the links form https://mcversions.net/
+#! NOTE: I get all the files from https://mcversions.net/
 #TODO: Add some config shit here
 
 #TEMP: BEGINING: All the code below this is temp and will be removed
-vanilla = {
-    
-    "1.17.1":'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
-    '1.17':'https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar',
-    
-}
+from bs4 import BeautifulSoup
+import requests
+
+download_pattern = 'https://mcversions.net/download/%s'
+
+needed_class = 'text-xs whitespace-nowrap py-3 px-8 bg-green-700 hover:bg-green-900 rounded text-white no-underline font-bold transition-colors duration-200'
+
+r = requests.get('https://mcversions.net/download/1.17.1')
+r2 = BeautifulSoup(r.text, features='html.parser')
+class__ = r2.find_all(class_=needed_class)
+print(class__)
 #TEMP: END: All the code form this line starts counting
