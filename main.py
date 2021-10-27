83f0cec7274adf4f565eb9b4ebb8fcaa043824e7
Advik <advik.b@gmail.com>
2021-10-27 17:57:00 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# importing GUI modules
+from tkinter import *
+from ttkthemes import themed_tk
+from tkinter import ttk
+
+# import the other stuff
+import os
+import confunc
