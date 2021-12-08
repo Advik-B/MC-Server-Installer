f61554f32e0ada6e48bfc3d44f5a9405a12eceae
Advik <advik.b@gmail.com>
2021-12-08 17:15:40 +0530

    testing comboboxes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from confunc import download_server_Tk, get_config
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QMessageBox, QFileDialog
-from PyQt5.QtGui import QIcon, QFont, QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QMessageBox, QFileDialog, QComboBox
+from PyQt5.QtGui import QIcon, QFont, QPixmap, QPalette
 from PyQt5.QtCore import Qt
+# from PyQt5 import Qt
 from sys import argv, exit as sys_exit
 import psutil
 import json
@@ -23,13 +24,32 @@ class MainWindow(QWidget):
     def initUI(self):
         """Initialize UI"""
         self.setWindowTitle('MC-Server-Installer')
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(300, 300, 1000, 600)
         self.setWindowIcon(QIcon('icon.ico'))
         self.background = QLabel(self)
         self.background.setPixmap(QPixmap('assets/pictures/background.png'))
         self.background.move(0, 0)
-        self.versionlbl = QLabel('Version:', self)        
+
+
+        self.versionlbl = QLabel('Version:', self)
+        self.versionlbl.setForegroundRole(QPalette.ColorRole.Button)
         self.versionlbl.move(20, 20)
+
+
+        self.versionscombo = QComboBox(self)
+        self.versionscombo.move(120, 20)
+        self.versionscombo.resize(200, 30)
+        self.versionscombo.setForegroundRole(QPalette.ColorRole.Highlight)
+        self.versionscombo.addItems(
+            [
+                '1.16.1',
+                '2',
+                '3',
+                '4',
+                '5',   
+            ]
+            )
+        
         self.setFont(QFont('Arial', 15))
         self.show()
 
